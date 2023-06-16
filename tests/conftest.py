00a6ae58e27370c0446536f552f4067bff92c011
HesterHobby <hesterlyons@gmail.com>
2023-06-16 21:17:48 +0100

    Force removal of all temp directories when tests finish.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,20 @@
 import sys
 import os
+import pytest
+import shutil
 
 # Add the parent directory to the module search path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-# Any common test fixtures or configuration can be placed here
+# This function is used to remove all temp files once pytest is finished.
+# It may be worth commenting out if debugging failures.
+@pytest.fixture(scope='session', autouse=True)
+def cleanup_temp_directories(request, tmp_path_factory):
+    # Get the temporary directory path
+    temp_dir = tmp_path_factory.getbasetemp().parent
+
+    yield
+
+    # Perform cleanup after all tests have finished
+    shutil.rmtree(temp_dir)
