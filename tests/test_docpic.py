9ca209662b7a7cdd3d53acc0a1ada7515c34b793
HesterHobby <hesterlyons@gmail.com>
2023-06-18 21:01:52 +0100

    Tidy up, trivial.

--- tests/test_docpic.py
+++ tests/test_docpic.py
@@ -3,11 +3,11 @@ import pytest
 from docpic_py.docpic import run_docpic
 from os import path
 
+
 @pytest.mark.slow
 def test_docpic_with_params(tmp_path):
     outfile = tmp_path / "test.md"
-    outimage = tmp_path / "img/test.png"
+    out_image = tmp_path / "img/test.png"
     run_docpic("../yaml_and_markup/example_md_config.md", str(outfile), "img")
     assert path.exists(outfile)
-    assert path.exists(outimage)
-
+    assert path.exists(out_image)
