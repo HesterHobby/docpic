131107b110a8234743c16f243ac90ffda19deb08
HesterHobby <hesterlyons@gmail.com>
2023-06-29 21:59:55 +0100

    Remaining tests for execute_step in selenium_processor.py - var_ref, click, clear, enter_text, select, wait and docpic.

--- tests/test_selenium_processor.py
+++ tests/test_selenium_processor.py
@@ -1,20 +1,18 @@
 import os
 import sys
+from unittest import mock
 
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, Mock, MagicMock
 from pytest_mock import MockFixture
 
 from selenium import webdriver
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.common.exceptions import UnexpectedTagNameException, NoSuchElementException
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
-from docpic_py.selenium_processor import get_element_from_varname, docpic, select, identify
+from docpic_py.selenium_processor import get_element_from_varname, docpic, select, identify, execute_step
 
 
 # Add the project root directory to the Python module search path
@@ -107,6 +105,22 @@ def test_select_with_dropdown_element(mocker: MockFixture):
     select_mock.select_by_visible_text.assert_called_once_with(labeltext)
 
 
+def test_select_with_missing_element(mocker):
+    # Arrange
+    element = mocker.Mock(spec=WebElement)
+    element.tag_name = 'select'
+    labeltext = 'Option 1'
+    select_mock = mocker.Mock(spec=Select)
+    select_mock.select_by_visible_text.side_effect = NoSuchElementException("Element not found")
+    mocker.patch('docpic_py.selenium_processor.Select', return_value=select_mock)
+
+    # Act/Assert
+    with pytest.raises(NoSuchElementException):
+        select(element, labeltext)
+
+    select_mock.select_by_visible_text.assert_called_once_with(labeltext)
+
+
 def test_select_with_non_dropdown_element(mocker):
     # Arrange
     element = mocker.Mock(spec=WebElement)
@@ -143,7 +157,7 @@ def test_identify_finds_element(mocker, varname):
 
 
 @pytest.mark.parametrize('varname', [None, 'element1'])
-def test_identify__does_not_find_element(mocker, varname):
+def test_identify_does_not_find_element(mocker, varname):
     # Arrange
     wait_mock = mocker.Mock(spec=WebDriverWait)
     selector = 'myElement'
@@ -161,3 +175,229 @@ def test_identify__does_not_find_element(mocker, varname):
 
         # Verify that module_vars is not modified when identification fails
         assert varname not in module_vars
+
+
+# Below here a bunch of stuff to test the integrated function
+# Mocked dependencies and functions
+@pytest.fixture
+def mock_identify(mocker):
+    return mocker.patch("docpic_py.selenium_processor.identify")
+
+
+@pytest.fixture
+def mock_get_element_from_varname(mocker):
+    return mocker.patch("docpic_py.selenium_processor.get_element_from_varname")
+
+
+@pytest.fixture
+def mock_click(mocker):
+    return mocker.patch("docpic_py.selenium_processor.click")
+
+
+@pytest.fixture
+def mock_clear(mocker):
+    return mocker.patch("docpic_py.selenium_processor.clear")
+
+
+@pytest.fixture
+def mock_enter_text(mocker):
+    return mocker.patch("docpic_py.selenium_processor.enter_text")
+
+
+@pytest.fixture
+def mock_select(mocker):
+    return mocker.patch("docpic_py.selenium_processor.select")
+
+
+@pytest.fixture
+def mock_driver_wait(mocker):
+    return mocker.patch("docpic_py.selenium_processor.driver_wait")
+
+
+@pytest.fixture
+def mock_docpic(mocker):
+    return mocker.patch("docpic_py.selenium_processor.docpic")
+
+
+# Mocked webdriver object
+@pytest.fixture
+def mock_webdriver():
+    return Mock()
+
+
+# Test cases
+def test_execute_step_identify(mock_identify, mock_get_element_from_varname):
+    # Arrange
+    step = {
+        "type": "identify",
+        "using": "id",
+        "selector": "myElement",
+        "var": "element1"
+    }
+
+    # Act
+    execute_step(step, mock_webdriver)
+
+    # Assert
+    mock_identify.assert_called_once()
+    assert mock_identify.call_args[0] == (mock.ANY, "id", "myElement", "element1")
+
+
+def test_execute_step_var_ref(mock_identify, mock_get_element_from_varname):
+    # Arrange
+    step = {
+        "type": "var-ref",
+        "var-name": "element1"
+    }
+
+    # Act
+    execute_step(step, mock_webdriver)
+
+    # Assert
+    assert not mock_identify.called
+    mock_get_element_from_varname.assert_called_once_with("element1")
+
+
+# Test case for step type "click"
+def test_execute_step_click(mock_identify, mock_get_element_from_varname, mock_webdriver):
+    # Arrange
+    step = {
+        "type": "click",
+        "target": {
+            "type": "identify",
+            "using": "id",
+            "selector": "myElement",
+            "var": None
+        }
+    }
+
+    # Act
+    execute_step(step, mock_webdriver)
+
+    # Assert
+    mock_identify.assert_called_once_with(mock.ANY, "id", "myElement", None)
+    assert not mock_get_element_from_varname.called
+
+
+# Test case for step type "clear"
+def test_execute_step_clear(mock_identify, mock_get_element_from_varname, mock_webdriver):
+    # Arrange
+    step = {
+        "type": "clear",
+        "target": {
+            "type": "var-ref",
+            "var-name": "element1"
+        }
+    }
+
+    # Act
+    execute_step(step, mock_webdriver)
+
+    # Assert
+    mock_get_element_from_varname.assert_called_once_with("element1")
+    assert not mock_identify.called
+
+
+# Test case for step type "enter-text"
+def test_execute_step_enter_text(mock_identify, mock_get_element_from_varname, mock_webdriver):
+    # Arrange
+    step = {
+        "type": "enter-text",
+        "target": {
+            "type": "identify",
+            "using": "id",
+            "selector": "myElement",
+            "var": None
+        },
+        "value": "Hello, World!"
+    }
+
+    # Create a mock for the WebElement
+    element_mock = Mock()
+
+    # Patch the identify function to return the mock element
+    mock_identify.return_value = element_mock
+
+    # Act
+    execute_step(step, mock_webdriver)
+
+    # Assert
+    mock_identify.assert_called_once_with(mock.ANY, "id", "myElement", None)
+    assert not mock_get_element_from_varname.called
+    element_mock.send_keys.assert_called_once_with("Hello, World!")
+
+
+def test_execute_step_select(mock_identify, mock_get_element_from_varname, mock_webdriver):
+    # Arrange
+    step = {
+        "type": "select",
+        "target": {
+            "type": "identify",
+            "using": "id",
+            "selector": "myElement",
+            "var": None
+        },
+        "value": "Option 1"
+    }
+
+    # Create a mock for the WebElement representing a dropdown
+    element_mock = mock_identify.return_value
+
+    # Mock the required methods for a dropdown element
+    element_mock.tag_name = "select"
+
+    # Create a list of mocked option elements
+    option_1 = mock.Mock(spec=WebElement)
+    option_1.text = "Option 1"
+    option_2 = mock.Mock(spec=WebElement)
+    option_2.text = "Option 2"
+    options = [option_1, option_2]
+
+    # Mock the find_elements method to return the list of options
+    element_mock.find_elements.return_value = options
+
+    # Act
+    execute_step(step, mock_webdriver)
+
+    # Assert
+    mock_identify.assert_called_once_with(mock.ANY, "id", "myElement", None)
+    assert not mock_get_element_from_varname.called
+    element_mock.find_elements.assert_called_once_with('xpath', './/option[normalize-space(.) = "Option 1"]')
+
+
+def test_execute_step_wait(mocker):
+    # Arrange
+    step = {
+        "type": "wait",
+        "value": 5
+    }
+    driver_mock = mocker.Mock()
+    wait_mock = mocker.patch("docpic_py.selenium_processor.time.sleep")
+
+    # Act
+    execute_step(step, driver_mock)
+
+    # Assert
+    wait_mock.assert_called_once_with(5)
+
+
+def test_execute_step_docpic(mocker):
+    # Arrange
+    step = {
+        "type": "docpic",
+        "outfile": "screenshot.png"
+    }
+    driver_mock = mocker.Mock(spec=webdriver.Chrome)
+    save_screenshot_mock = mocker.patch.object(driver_mock, "save_screenshot")
+    exists_mock = mocker.patch("os.path.exists")
+
+    # Act
+    execute_step(step, driver_mock)
+
+    # Assert
+    save_screenshot_mock.assert_called_once_with("screenshot.png")
+    exists_mock.assert_called_once_with("screenshot.png")
+
+# Add test cases for other step types (wait, docpic)
+# Add test case for invalid step type
+
