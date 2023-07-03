f99c4bc2843fa61f6bb69aa7737a9d5fd3399c3c
HesterHobby <hesterlyons@gmail.com>
2023-07-03 22:11:20 +0100

    New test for command line execution; weirdly only runs from command line pytest.

--- tests/test_docpic.py
+++ tests/test_docpic.py
@@ -1,5 +1,7 @@
 # A single test to make sure it still runs.
 import pytest
+import subprocess
+
 from docpic_py.docpic import run_docpic
 from os import path
 
@@ -12,4 +14,15 @@ def test_docpic_with_params(tmp_path):
     assert path.exists(outfile)
     assert path.exists(out_image)
 
-# Todo: Put in a test for command line.
+
+# Note this only runs from the command line, the IDE gives a weird error.
+def test_run_from_command_line():
+    # Define the command to run your process
+    command = "python ../docpic_py/docpic.py --infile ../yaml_and_markup/example_md_config.md --img-dir assets/"
+
+    # Run the command and capture the output
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+    # Assert the expected output or behavior
+    assert result.returncode == 0  # Check if the process exited successfully
+    assert "Output is in" in result.stdout  # Check if the expected output message is present
